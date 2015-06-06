34b515301012deafcd4da498f11c951ac56620eb
wugensan <wugensan99@163.com>
2015-06-06 14:04:17 +0800

    commit all files

---
+++ rango/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import RequestContext
+
+def index(request):
+    context = RequestContext(request)
+    context_dict = {'boldmessage':'I am bold font from the context'}
+    return render_to_response('rango/index.html',context_dict,context)
